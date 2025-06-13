3c27d94033a5f1c96ea725cd49e63d0c4a424b42
João Rosiak <contato.rosiak@gmail.com>
2025-06-13 20:04:23 -0300

    Primeiro commit do backend da Jobu, comecando a desenvolver a API

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+
+const userRoutes = require('./routes/users')
+const postRoutes = require('./routes/posts')
+const serviceRoutes = require('./routes/servicos')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/users', userRoutes)
+app.use('/posts', postRoutes)
+app.use('/servicos', serviceRoutes)
+
+app.listen(3000, () => {
+  console.log('API rodando em http://localhost:3000')
+
