0d489eff6ee7905f2cbdd42601a6017ba7da59fe
João Rosiak <contato.rosiak@gmail.com>
2025-06-16 20:43:30 -0300

    Ajustado o arquivo dotenv para ocultar dados sensíveis da pasta raíz do projeto, ajustado o segredo do jwt

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const cors = require('cors')
 
 const userRoutes = require('./routes/users')
-const postRoutes = require('./routes/posts')
 const serviceRoutes = require('./routes/servicos')
 
 const app = express()
@@ -11,7 +10,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/users', userRoutes)
-app.use('/posts', postRoutes)
 app.use('/servicos', serviceRoutes)
 
 app.listen(3000, () => {
--- services/firebaseCredentials.js
+++ services/firebaseCredentials.js
@@ -1,17 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../config.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyA3ubledXpFwJSY5_07WVuIfMrOlUVMA9c",
-    authDomain: "tesla-9ba1a.firebaseapp.com",
-    projectId: "tesla-9ba1a",
-    storageBucket: "tesla-9ba1a.firebasestorage.app",
-    messagingSenderId: "147978344945",
-    appId: "1:147978344945:web:15ba16d1477999963b0c50",
-};
+
+// coloquei dentro do dotenv para melhorar a segurança
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
