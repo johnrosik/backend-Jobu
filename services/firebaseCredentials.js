e1d22ee5c93e471a4345b972817d47c9f673b677
João Rosiak <contato.rosiak@gmail.com>
2025-06-16 18:50:36 -0300

    Adicionado as credenciais do Firebase e instalado o pacote do firebase

---
+++ services/firebaseCredentials.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA3ubledXpFwJSY5_07WVuIfMrOlUVMA9c",
+    authDomain: "tesla-9ba1a.firebaseapp.com",
+    projectId: "tesla-9ba1a",
+    storageBucket: "tesla-9ba1a.firebasestorage.app",
+    messagingSenderId: "147978344945",
+    appId: "1:147978344945:web:15ba16d1477999963b0c50",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
